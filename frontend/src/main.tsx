dc330e3f35db18a099af6e5911015057be0a4e35
ajaymude <mudeajay@gmail.com>
2024-09-24 23:51:57 +0530

    update route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./global.css"
-import { BrowserRouter } from "react-router-dom"
-import AppRoutes from "./AppRoutes"
-import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { Toaster } from "sonner"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-})
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -21,13 +21,9 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
-
           <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
-)
-
-
-// route setup 
+);
